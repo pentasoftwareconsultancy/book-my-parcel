ca8b7cf07b0a8db540c91a3b686c4c43e5a420af
nexusctc <hr@nexusctc.com>
2026-06-25 17:58:47 +0530

    changes at cashfree

--- BMP-Backend/src/config/cashfree.client.js
+++ BMP-Backend/src/config/cashfree.client.js
@@ -3,9 +3,11 @@ import axios from "axios";
 const CASHFREE_API_VERSION = "2023-08-01";
 
 function getCashfreeBaseUrl() {
-  return process.env.CASHFREE_ENV === "PRODUCTION"
-    ? "https://api.cashfree.com/pg"
-    : "https://sandbox.cashfree.com/pg";
+  // return process.env.CASHFREE_ENV === "PRODUCTION"
+  //   ? "https://api.cashfree.com/pg"
+  //   : "https://sandbox.cashfree.com/pg";
+
+  return "https://api.cashfree.com/pg";
 }
 
 function getCashfreeHeaders() {
